e00b16137ee9315fca2d3c72db278aec9adc74ce
SaintHazzard <chamitos1998@hotmail.com>
2024-04-13 08:40:02 -0500

    feat: :tada: Crud basico Spring boot
    Pryecto simple de spring boot, CRUD con postgreSQL
    
    HIBERNATE

---
+++ src/test/java/com/hazzard/holamundo/holaspringboot/HolaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazzard.holamundo.holaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
